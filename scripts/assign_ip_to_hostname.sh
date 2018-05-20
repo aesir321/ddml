737d376db88101b9140339f619b52a84d8a0ec00
Will Parker <parkerwill625@gmail.com>
2018-05-20 15:53:40 +0200

    Added script to copy master ssh key over.  Added groups to hosts file.  Updates ip script.

--- scripts/assign_ip_to_hostname.sh
+++ scripts/assign_ip_to_hostname.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-PASSWORD=mastergroup3
-HOSTNAME=mastergroup3
+HOSTNAME=
 IP_ADDRESS=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1 -d'/')
-CMD="http://dyndns.lrz.de/cgi/update.py?hostname=${HOSTNAME}&user=${HOSTNAME}&pass=${PASSWORD}&domain=dyn.mwn.de&myip=${IP_ADDRESS}"
+CMD="http://dyndns.lrz.de/cgi/update.py?hostname=${HOSTNAME}&user=${HOSTNAME}&pass=${HOSTNAME}&domain=dyn.mwn.de&myip=${IP_ADDRESS}"
 echo ${CMD}
 info=$(curl "${CMD}")
 echo $info
