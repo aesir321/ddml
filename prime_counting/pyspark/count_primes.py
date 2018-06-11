ce4828155d76296015d6d3ec995856db9c1483c4
parkerwill <will@will-parker.co.uk>
2018-06-11 14:08:13 +0200

    Update count_primes.py
    Update to use DataFrame

--- prime_counting/pyspark/count_primes.py
+++ prime_counting/pyspark/count_primes.py
@@ -25,12 +25,12 @@ if __name__ == "__main__":
                         if n % (f + 2) == 0: return False
                         f +=6
                 return True
-        
+
         is_prime_udf = udf(is_prime)
 
-        n = 1 #int(sys.argv[1]) if len(sys.argv) > 1 else 9
-        numbers = [1, 2]
-        numbers.extend(spark.xrange(3, long(10 ** n), step=2))
+        n = int(sys.argv[1]) if len(sys.argv) > 1 else 1
+        numbers = spark.range(1, 3)
+        numbers = numbers.union(spark.range(3, long(10 ** n), step=2))
         primes = numbers.withColumn('is_prime', is_prime_udf('id').cast(LongType()))
         prime_count = primes.agg({'is_prime':'sum'})
         prime_count.write.csv('output/pyspark_10e_' + str(n))
