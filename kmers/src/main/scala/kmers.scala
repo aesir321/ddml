9cecb1f0f9693b33bfc552d01f554ffd6f0aafe0
Will Parker <parkerwill625@gmail.com>
2018-06-22 21:22:25 +0200

    Optimised.

--- kmers/src/main/scala/kmers.scala
+++ kmers/src/main/scala/kmers.scala
@@ -4,7 +4,7 @@ import org.apache.spark.SparkContext
 object kmers {
 	def main(args: Array[String]): Unit = {
 		if (args.size < 3) {
-			println("Usage: <Kmer fasta file path> <K> <N>")
+			println("Usage: <Kmer fasta file path> <Output file path> <K>")
 			sys.exit(1)
 		}
 		
@@ -12,20 +12,17 @@ object kmers {
 		val sc = new SparkContext(sparkConf)
 		
 		val input = args(0)
-		val K = args(1).toInt
-		val N = args(2).toInt
+		val output = args(1)
+		val K = args(2).toInt
 
 		val bcK = sc.broadcast(K)
-		val bcN = sc.broadcast(N)
 		val data = sc.textFile(input)
 		
-		// BAd naming here, think it will clash with object name, so capitalised the K.
+		// Bad naming here, think it will clash with object name, so capitalised the K.
 		val Kmers = data.flatMap(_.sliding(bcK.value, 1).map((_, 1))) 
 		val groupedKmers = Kmers.reduceByKey(_ + _)
-		val partitions = groupedKmers.mapPartitions(_.toList.sortBy(_._2).takeRight(bcN.value).toIterator)
-		val allTopN = partitions.sortBy(_._2, false, 1).take(bcN.value)
-
-		allTopN.foreach(println)
+		val partitions = groupedKmers.mapPartitions(_.toList.sortBy(_._2).toIterator)
+		partitions.saveAsTextFile(output)
 
 		sc.stop()
 	}
